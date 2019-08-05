88aaa9b48a23a5dd7f4b568fda0493651e532470
Filip <rj23@protonmail.com>
2019-08-05 23:06:33 +0200

    feat: Photo and sticker support

--- telegram.py
+++ telegram.py
@@ -7,7 +7,8 @@ from aiogram import Bot, Dispatcher, executor, types
 _loop = asyncio.get_event_loop()
 
 conf_fields = [{'type': 'STRING', 'hint': 'Your telegram bot token',
-                            'defaultValue': '', 'optional': False, 'mask': True}]
+                'defaultValue': '', 'optional': False, 'mask': True}]
+
 
 class TelegramService(ChatPlugService):
     async def on_message_received(self, msg):
@@ -16,7 +17,7 @@ class TelegramService(ChatPlugService):
         await self.bot.send_message(int(target_id), "*" + message["author"]["username"] + "*: " + message["body"], parse_mode="Markdown")
         for attachment in message["attachments"]:
             await self.bot.send_photo(int(target_id), attachment["sourceUrl"])
-    
+
     async def on_configuration_received(self, conf):
         with open('config.' + self.instance_id + '.json', 'w') as outfile:
             json.dump({'botToken': conf['fieldValues'][0]}, outfile)
@@ -34,32 +35,57 @@ class TelegramService(ChatPlugService):
         config = self.get_config()
         self.bot = Bot(token=config["botToken"])
         self.dp = Dispatcher(self.bot)
-        self.dp.register_message_handler(self.handleMessage)
+        self.dp.register_message_handler(self.handleTelegramMessage, content_types=[
+                                         types.ContentType.PHOTO, types.ContentType.TEXT, types.ContentType.STICKER])
         await self.dp.start_polling()
-    
-    async def handleMessage(self, message):
+
+    async def handleTelegramMessage(self, message):
+        # Cache user profile images
         if not str(message["from"]["id"]) in self.user_photos:
             pics = await self.bot.get_user_profile_photos(message["from"]["id"])
             file = await self.bot.get_file(pics["photos"][0][-2]["file_id"])
             file_path = self.bot.get_file_url(file["file_path"])
             self.user_photos[str(message["from"]["id"])] = file_path
 
-        await self.send_message(
-            message["text"],
-            str(message["message_id"]), 
-            str(message["chat"]["id"]), 
-            message["from"]["username"], 
-            str(message["from"]["id"]), 
-            self.user_photos[str(message["from"]["id"])], [])
-        print(message)
+        attachments = []
+        # Handle photos
+        if "photo" in message: # message["photo"] != null 
+            file = await self.bot.get_file(message["photo"][-1]["file_id"])
+            file_path = self.bot.get_file_url(file["file_path"])
+            attachments = [{
+                "sourceUrl": file_path,
+                "type": "IMAGE",
+                "originId": message["photo"][-1]["file_id"], 
+            }]
 
+        # Handle stickers
+        if "sticker" in message: # message["sticker"] != null 
+            file = await self.bot.get_file(message["sticker"]["file_id"])
+            file_path = self.bot.get_file_url(file["file_path"])
+            attachments = [{
+                "sourceUrl": file_path,
+                "type": "IMAGE",
+                "originId": message["sticker"]["file_id"], 
+            }]
+
+        await self.send_message(
+            message["text"] or "",  # body
+            str(message["message_id"]),  # originId
+            str(message["chat"]["id"]),  # originThreadId
+            message["from"]["username"],  # author.username
+            str(message["from"]["id"]),  # author.originId
+            self.user_photos[str(message["from"]["id"])],  # author.avatarUrl
+            attachments  # attachments
+        )
 
     def get_config(self):
         with open('config.' + self.instance_id + '.json') as json_file:
             data = json.load(json_file)
             return data
 
-cp = TelegramService(os.environ["INSTANCE_ID"], os.environ["WS_ENDPOINT"], os.environ["HTTP_ENDPOINT"])
+
+cp = TelegramService(
+    os.environ["INSTANCE_ID"], os.environ["WS_ENDPOINT"], os.environ["HTTP_ENDPOINT"])
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(cp.connect())
